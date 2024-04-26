fec0fa71ca025d073d5e3e6d7a3df6ba42a0da17
Antonio Augusto <contatoaugusto@gmail.com>
2024-04-26 14:07:31 -0300

    Melhorias - 2024-04-26

--- projeto/src/javascript/header.js
+++ projeto/src/javascript/header.js
@@ -10,4 +10,18 @@ $(document).ready(function() {
     if (mensagemAlert.html().trim().length > 0 )
         mensagemAlert.show();
 
+
+    // Script para controlar a exibição dos submenus
+    $('.submenu-toggle').click(function(e) {
+        e.preventDefault();
+        var submenu = $(this).attr('href');
+        $(submenu).toggleClass('show');
+        
+        var submenuCadastro = $("#iSetaAlternada");
+
+        if (submenuCadastro.hasClass('fa-chevron-down'))
+            submenuCadastro.removeClass('fa-chevron-down').addClass('fa-chevron-up');
+        else
+            submenuCadastro.removeClass('fa-chevron-up').addClass('fa-chevron-down');
+    });
 });
